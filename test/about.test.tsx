b6ba372bf55caed28d1fa5fa784ba31ce98616e3
Daniel <116294062+VBNetEnjoyer@users.noreply.github.com>
2025-07-12 18:16:05 +0300

    Пофиксил тесты

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { About } from "../src/client/pages/About";
+import {render} from "@testing-library/react";
+import {About} from "../src/client/pages/About";
 
 describe("About", () => {
 	it("Should render author name", () => {
-		const { container } = render(<About />);
+		const {container} = render(<About />);
 
-		expect(container.textContent).toBe("AboutAuthor: [Youdr Name]");
+		expect(container.textContent).toBe("AboutAuthor: Maltsev Daniil");
 	});
 });
