6d3a1ab1bfd1424c0fddce88709c5fc0e0abd86e
Daniel <116294062+VBNetEnjoyer@users.noreply.github.com>
2025-07-13 17:21:32 +0300

    Добавил изображения в карточки товаров

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,34 +1,35 @@
 export interface ProductShortInfo {
-    id: number;
-    name: string;
-    price: number;
+	id: number;
+	name: string;
+	price: number;
+	img?: string;
 }
 
 export interface Product extends ProductShortInfo {
-    description: string;
-    material: string;
-    color: string;
+	description: string;
+	material: string;
+	color: string;
 }
 
 export interface CheckoutFormData {
-    name: string;
-    phone: string;
-    address: string;
+	name: string;
+	phone: string;
+	address: string;
 }
 
 export interface CartItem {
-    name: string;
-    price: number;
-    count: number;
+	name: string;
+	price: number;
+	count: number;
 }
 
 export type CartState = Record<number, CartItem>;
 
 export interface Order {
-    form: CheckoutFormData;
-    cart: CartState;
+	form: CheckoutFormData;
+	cart: CartState;
 }
 
 export interface CheckoutResponse {
-    id: number;
+	id: number;
 }
